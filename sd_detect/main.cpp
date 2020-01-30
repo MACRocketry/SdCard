9f1d0c4d1e71527518e3b779aa028e1ad780fc95
Jerry Pham <chalizardmisadon@yahoo.com>
2020-01-30 01:39:00 -0500

    add comment for sd_detect test file

--- sd_detect/main.cpp
+++ sd_detect/main.cpp
@@ -7,13 +7,17 @@ void setup()
 {
 	Serial.begin(9600);
 	while (!Serial);
-	pinMode(LED_BUILTIN, OUTPUT);
+	
 	bool led_on = true;
-
 	bool ret = SD.begin(10);
+
+	// note: LED_BUILTIN shares the same pin for one of the SPI 0 line,
+	//       so SD.begin will set pinMode() for LED_BUILTIN. Since we
+	//       are not using SD anymore, we can overwrite pinMode()
+	pinMode(LED_BUILTIN, OUTPUT);
 	while (true) {
-		if (ret) Serial.print("Fail to init SD\n");
-		else     Serial.print("Init SD success!\n");
+		if (ret) Serial.print("Init SD success!\n");
+		else     Serial.print("Fail to init SD\n");
 		digitalWrite(LED_BUILTIN, led_on = !led_on);
 		delay(500);
 	}	
